c9db174a182c42d7babcf95ef951e42d3403ae16
Md Azaz <125148564+azaz9026@users.noreply.github.com>
2023-07-27 23:50:45 +0530

    Update script.js
    some changes in script and gsap

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const locoScroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true
 });
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
 locoScroll.on("scroll", ScrollTrigger.update);
 
 // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
